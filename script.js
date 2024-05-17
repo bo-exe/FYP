a9dfee1fea6277478fc0781706b41f0bb1a041a8
bo-exe <22020749@myrp.edu.sg>
2024-05-17 14:52:47 +0800

    QR draft 1

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ function toggleMenu() {
     var navbar = document.querySelector('.navbar');
     navbar.classList.toggle('responsive');
 }
+
+// QR Code
+
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+ 
+domReady(function () {
+ 
+    // If found you qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        alert("You Qr is : " + decodeText, decodeResult);
+    }
+ 
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
+});

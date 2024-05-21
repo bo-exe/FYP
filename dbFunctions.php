154cd5129e90f0bd81045e5070827dcc0ac469cc
ashhhlolol <22000765@myrp.edu.sg>
2024-05-21 13:20:50 +0800

    change dbFunctions.php

--- dbFunctions.php
+++ dbFunctions.php
@@ -1,15 +1,9 @@
 <?php
-$db_host = "auth-db994.hstgr.io";  // Database server address
-$db_user = "root";                 // Database username
-$db_pass = "";                     // Database password (empty in this case)
-$db_name = "u877255869_FYP_2024_01_DB";  // Database name
+$db_host = "auth-db994.hstgr.io";
+$db_user = "root";                 
+$db_pass = "";                     
+$db_name = "u877255869_FYP_2024_01_DB";  
 
 // Attempt to connect to the MySQL database
 $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-// Check if the connection was successful
-if (!$link) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
 ?>

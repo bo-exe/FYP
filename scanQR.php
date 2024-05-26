d73c73bf9f1a89781be9d822745805035c041ee2
bo-exe <22020749@myrp.edu.sg>
2024-05-26 14:43:04 +0800

    tried to change .html to .php

---
+++ scanQR.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR Scanning Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="qr-container">
+        <h1>Scan QR Codes</h1>
+        <div class="qr-section">
+            <div id="qr-scanner"></div>
+        </div>
+    </div>
+    <script src="html5-qrcode.min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>

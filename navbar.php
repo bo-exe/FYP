31499ccb089171a95ca492bfda0d25e7d8cf6e55
bo-exe <22020749@myrp.edu.sg>
2024-05-26 15:12:44 +0800

    FIXED NAVBAR

--- navbar.php
+++ navbar.php
@@ -14,23 +14,6 @@
         <a href="#services">Services</a>
         <a href="#contact">Contact</a>
     </div>
-
-    <div id="home" style="padding:20px;">
-        <h1>Home</h1>
-        <p>Welcome to the home page!</p>
-    </div>
-    <div id="about" style="padding:20px;">
-        <h1>About</h1>
-        <p>Learn more about us here.</p>
-    </div>
-    <div id="services" style="padding:20px;">
-        <h1>Services</h1>
-        <p>These are the services we offer.</p>
-    </div>
-    <div id="contact" style="padding:20px;">
-        <h1>Contact</h1>
-        <p>Get in touch with us.</p>
-    </div>
     <script src="script.js"></script>
 </body>
 </html>

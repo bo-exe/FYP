cf743901499aaaa968083f6f814b8d7def0f2f42
Leia-Ang <22011199@myrp.edu.sg>
2024-05-29 14:03:58 +0800

    updated signup

--- Signup.php
+++ Signup.php
@@ -7,24 +7,25 @@
     <link rel="stylesheet" href="style.css">
     <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
     <link rel="icon" type="image/x-icon" href="images/logo.jpg">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
     <div class="signup-container">
         <form method="post" action="doSignup.php">
             <h2 class="text-center mb-4">Sign Up</h2>
             <div class="form-group">
-                <label for="uname">Username</label>
-                <input type="text" class="form-control" placeholder="Enter Username" name="username" required>
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Enter Username" name="username" required>
             </div>
 
             <div class="form-group">
-                <label for="psw">Password</label>
-                <input type="password" class="form-control" placeholder="Enter Password" name="password" required>
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter Password" name="password" required>
             </div>
 
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" class="form-control" placeholder="Enter Email" name="email" required>
+                <input type="email" class="form-control" id="email" placeholder="Enter Email" name="email" required>
             </div>
 
             <div class="text-center">
@@ -35,5 +36,7 @@
             </div>
         </form>
     </div>
+
+
 </body>
 </html>

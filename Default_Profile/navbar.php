d70d476df038e5bc45dc595c8a285da7af4f6c84
bo-exe <131931533+bo-exe@users.noreply.github.com>
2024-05-29 13:26:14 +0800

    .

---
+++ Default_Profile/navbar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation Bar</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <!-- Navbar Section -->
+    <div class="header">
+        <nav class="navbar navbar-expand-sm sticky-top">
+            <a class="navbar-brand" href="#">
+                <img src="images/logo.png" alt="logo" style="width:100px;" class="logo">
+            </a>
+            <div class="nav-items" style="display: none;">
+                <a class="nav-item active nav-mobile-item" href="#home"><i class='bx bx-home'></i> Home</a>
+                <a class="nav-item nav-mobile-item" href="#activities"><i class='bx bx-user'></i> Activities</a>
+                <a class="nav-item nav-mobile-item" href="#redeem"><i class='bx bx-phone'></i> Redeem</a>
+                <a class="nav-item nav-mobile-item" href="#stores"><i class='bx bx-user'></i> Stores</a>
+            </div>
+            <div class="nav-item hamburger"><i class='bx bx-menu'></i></div>
+        </nav>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>

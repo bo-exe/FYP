10ce669938773edcdb8ec584af5c524fb0d3eade
bo-exe <22020749@myrp.edu.sg>
2024-06-04 18:46:34 +0800

    .

--- dbFunctions.php
+++ dbFunctions.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "localhost";
+$db_host = "auth-db994.hstgr.io";
 $db_user = "root";                 
 $db_pass = "";                     
-$db_name = "u877255869_fyp_2024_01_db";  
+$db_name = "u877255869_FYP_2024_01_DB";
 
 // Attempt to connect to the MySQL database
 $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);

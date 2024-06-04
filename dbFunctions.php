07edc9b1b1acb6e362a87ad3a56860ac2b6fbadc
bo-exe <22020749@myrp.edu.sg>
2024-06-04 18:50:10 +0800

    name change fix stuff

--- dbFunctions.php
+++ dbFunctions.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = "auth-db994.hstgr.io";
-$db_user = "root";                 
-$db_pass = "";                     
+$db_user = "u877255869_User";                 
+$db_pass = "C300_OneRP";                     
 $db_name = "u877255869_FYP_2024_01_DB";
 
 // Attempt to connect to the MySQL database

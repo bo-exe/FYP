d7146ed95c27f4ec09d3b9e96b4ad8531d50157f
bo-exe <22020749@myrp.edu.sg>
2024-06-26 19:53:52 +0800

    .

--- admin_gigOverview.php
+++ admin_gigOverview.php
@@ -37,7 +37,7 @@ $link->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Gigs</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="volunteeradminstyle.css">
 </head>
 
 <body>

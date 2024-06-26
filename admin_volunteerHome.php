ab455916fe4b04bc50c2d4f8deb899792d250daa
bo-exe <22020749@myrp.edu.sg>
2024-06-26 20:12:29 +0800

    .

--- admin_volunteerHome.php
+++ admin_volunteerHome.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Volunteer Admin Home</title>
-    <link rel="stylesheet" type="text/css" href="volunteeradminstyle.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <br>
 <?php include "admin_volunteerNavbar.php"; ?>

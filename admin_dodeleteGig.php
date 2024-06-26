beaba3e646a616a4c93e5f94c1bb9b73a5d1dd63
bo-exe <22020749@myrp.edu.sg>
2024-06-26 10:28:42 +0800

    .

--- admin_dodeleteGig.php
+++ admin_dodeleteGig.php
@@ -3,11 +3,11 @@ include "dbFunctions.php";
 include "admin_volunteerNavbar.php";
 include "ft.php";
 
-if (isset($_GET['EventID'])) {
-    $EventID = $_GET['EventID'];
+if (isset($_GET['eventID'])) {
+    $EventID = $_GET['eventID'];
 
     $msg = "";
-    $query = "DELETE FROM events WHERE EventID=?";
+    $query = "DELETE FROM events WHERE eventID=?";
     $stmt = mysqli_prepare($link, $query);
     mysqli_stmt_bind_param($stmt, "i", $EventID);
     $result = mysqli_stmt_execute($stmt);

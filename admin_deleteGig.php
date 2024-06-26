68cbb21af00672d2e1b88f2a5ef4298322108eee
bo-exe <22020749@myrp.edu.sg>
2024-06-26 19:35:25 +0800

    .

--- admin_deleteGig.php
+++ admin_deleteGig.php
@@ -21,7 +21,10 @@ if (isset($_GET['eventID'])) {
         $locations = $row['locations'];
         $descs = $row['descs'];
         $points = $row['points'];
-        $image = $row['images'];
+        // Retrieve the image binary data from database
+        $imageData = $row['images'];
+        // Convert binary data to base64 encoded string
+        $image = base64_encode($imageData);
     }
 
 } else {
@@ -31,17 +34,17 @@ if (isset($_GET['eventID'])) {
 ?>
 
 <html>
-
 <head>
     <meta charset="UTF-8">
     <title>Delete Gig</title>
     <link rel="stylesheet" type="text/css" href="volunteeradminstyle.css">
 </head>
 <body>
-<div class="deletegig-container">
+    <div class="deletegig-container">
         <?php if (!empty($eventID)) { ?>
             <div class="card">
-                <img src="Images/<?php echo $image; ?>" alt="Event Image">
+                <!-- Use data URI scheme to embed image -->
+                <img src="data:image/jpeg;base64,<?php echo $image; ?>" alt="Event Image">
                 <h2><?php echo $title; ?></h2>
                 <p><b>Start Date:</b> <?php echo $dateTimeStart; ?></p>
                 <p><b>End Date:</b> <?php echo $dateTimeEnd; ?></p>
@@ -50,12 +53,12 @@ if (isset($_GET['eventID'])) {
                 <p><b>Points:</b> <?php echo $points; ?></p>
                 <a href="admin_dodeleteGig.php?eventID=<?php echo $eventID; ?>" class="del-btn">Delete</a>
             </div>
-    <?php } else { ?>
-        <div style="text-align: center;">
-            <p>Invalid Event ID. Please try again.</p>
-            <p><a href="admin_allGigs.php">Back to Gigs.</a></p>
-        </div>
-    <?php } ?>
+        <?php } else { ?>
+            <div style="text-align: center;">
+                <p>Invalid Event ID. Please try again.</p>
+                <p><a href="admin_allGigs.php">Back to Gigs.</a></p>
+            </div>
+        <?php } ?>
+    </div>
 </body>
-
 </html>

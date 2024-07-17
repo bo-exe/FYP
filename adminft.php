7a21898d6f82bc1337a1d441e3890dd8bccf1bb4
bo-exe <22020749@myrp.edu.sg>
2024-07-17 09:55:58 +0800

    footer

---
+++ adminft.php
@@ -0,0 +1,3 @@
+<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/css/boxicons.min.css">
+<link rel="icon" type="image/x-icon" href="images/logo.jpg">

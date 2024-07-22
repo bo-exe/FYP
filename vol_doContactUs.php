d2948b12ca92b685c2d39b83da4e55dea5e9dfaf
nicholas <22014726@myrp.edu.sg>
2024-07-22 12:24:52 +0800

    updates

--- vol_doContactUs.php
+++ vol_doContactUs.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Feedback</title>
+    <title>Contact us Successful</title>
     <link rel="icon" type="image/x-icon" href="images/logo.jpg">
     <link rel="stylesheet" href="style.css">
     <style>

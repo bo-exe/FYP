e1454f796a49aa5cae18edf1405654b0bdc63474
bo-exe <22020749@myrp.edu.sg>
2024-07-22 16:25:10 +0800

    .

--- admin_volProfile.php
+++ admin_volProfile.php
@@ -21,7 +21,6 @@ if (mysqli_num_rows($result) == 1) {
     $row = mysqli_fetch_assoc($result);
     $username = $row['username'];
     $email = $row['email'];
-    $password = $row['password'];
     $profile_pic = $row['profile_pic']; 
 } else {
     // Handle error if user data not found
@@ -51,35 +50,12 @@ if (mysqli_num_rows($result) == 1) {
             <img src="images/<?php echo htmlspecialchars($profile_pic); ?>" alt="Profile Picture" class="profile-picture">
             <p><strong>Username:</strong> <?php echo htmlspecialchars($username); ?></p>
             <p><strong>Email:</strong> <?php echo htmlspecialchars($email); ?></p>
-            <p><strong>Password: </strong></p>
-            <div class="password-container">
-                <input type="password" id="password" value="<?php echo htmlspecialchars($password); ?>" readonly>
-                <span class="toggle-password" onclick="togglePassword()">
-                    <i class="fas fa-eye"></i>
-                </span>
-            </div>
         </div>
         
         <div class="text-center">
             <a href="admin_volunteerEditProfile.php" class="btn-edit-profile">Edit Profile</a>
+            <a href="vol_forgotPassword.php" class="btn-edit-profile">Change Password</a>
         </div>
     </div>
-
-    <script>
-        function togglePassword() {
-            var passwordField = document.getElementById("password");
-            var passwordFieldType = passwordField.getAttribute("type");
-            var toggleIcon = document.querySelector(".toggle-password i");
-            if (passwordFieldType == "password") {
-                passwordField.setAttribute("type", "text");
-                toggleIcon.classList.remove("fa-eye");
-                toggleIcon.classList.add("fa-eye-slash");
-            } else {
-                passwordField.setAttribute("type", "password");
-                toggleIcon.classList.remove("fa-eye-slash");
-                toggleIcon.classList.add("fa-eye");
-            }
-        }
-    </script>
 </body>
 </html>
